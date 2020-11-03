b4bbbbeba329ed37ea31ab674536687f79824058
unknown <miyashaheen1995@gmail.com>
2020-11-03 18:47:46 +0530

    Modify:functionalities

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,22 +2,17 @@
 
 echo "Welcome to Employee Wage Computation!!!"
 
-#check employee attendance
-if [ $(( RANDOM%2  )) == 1 ]
-then
-	echo "Employee is present"
-else
-	echo "Employee is absent"
-fi
+#constants
+EMP_PART_TIME=1
+EMP_FULL_TIME=2
+EMP_RATE_PER_HOUR=20
 
+#variables
 
-#calculate daily employee wage
-wage_per_hour=20
-total_working_hours=8
-daily_wage=$(( wage_per_hour * total_working_hours ))
+case $(( RANDOM%3)) in
+			$EMP_PART_TIME) working_hours=4;;
+			$EMP_FULL_TIME) working_hours=8;;
+				     *) working_hours=0;;
+esac
+daily_wage=$(( working_hours*EMP_RATE_PER_HOUR ))
 echo "Daily Employee Wage="$daily_wage
-
-#part time employee and wage
-part_time_working_hours=4
-part_time_wage=$(( wage_per_hour*part_time_working_hours ))
-echo "Part Time Employee Wage="$part_time_wage
