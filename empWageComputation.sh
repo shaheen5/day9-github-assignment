9d09486fb615fe8978c2f85c48bca12cdbbc140b
unknown <miyashaheen1995@gmail.com>
2020-11-03 16:40:37 +0530

    Added:functionalities

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,11 @@
-#!/bin/bash
+#!/bin/bash -x
 
 echo "Welcome to Employee Wage Computation!!!"
+
+#check employee attendance
+if [ $(( RANDOM%2  )) == 1 ]
+then
+	echo "Employee is present"
+else
+	echo "Employee is absent"
+fi
