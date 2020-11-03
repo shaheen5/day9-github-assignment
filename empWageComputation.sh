cbb72f1f5cfd6233d720d5249a99a1b13966657b
unknown <miyashaheen1995@gmail.com>
2020-11-03 18:22:18 +0530

    Added:functionalities

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -10,9 +10,14 @@ else
 	echo "Employee is absent"
 fi
 
+
 #calculate daily employee wage
 wage_per_hour=20
 total_working_hours=8
 daily_wage=$(( wage_per_hour * total_working_hours ))
 echo "Daily Employee Wage="$daily_wage
 
+#part time employee and wage
+part_time_working_hours=4
+part_time_wage=$(( wage_per_hour*part_time_working_hours ))
+echo "Part Time Employee Wage="$part_time_wage
